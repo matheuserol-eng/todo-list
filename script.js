7580875716d0ec2d92cef663be980e7319f94fd5
Matheus <matheuserol@gmail.com>
2025-09-15 19:59:42 -0300

    Criando o arquivo javascript

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const li = document.createElement("li")
 
 li.textContent = "Estudar com os materiais do classRoom"
 list.append(li)
+
+list.addEventListener("click", event => {
+    const li = event.target.closest("li")
+    
+    if(!li) return
+
+        li.classList.toggle("done")
+})
